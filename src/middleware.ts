4031c446331b7fbcfd7d7170d1cb767a595ff0ba
Saad575757 <smartysaad59@gmail.com>
2026-07-05 00:21:21 +0500

    pricing issue fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-up(.*)",
   "/api/(.*)",
   "/sitemap.xml",
-  "/robots.txt",
+  
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
@@ -14,7 +14,7 @@ export default clerkMiddleware(async (auth, req) => {
     await auth.protect();
   }
 });
-
+//saad wazir
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
